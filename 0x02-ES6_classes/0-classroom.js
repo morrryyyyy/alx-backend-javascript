b8170c99dc957e204b64ba88a8c907a7f8ad3438
Maryam <morrryyyy21@gmail.com>
2024-10-03 19:46:18 +0100

    create classroom class

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,8 @@
+// Define the ClassRoom class and export it as default
+export default class ClassRoom {
+  // Constructor to initialize maxStudentsSize
+  constructor(maxStudentsSize) {
+    // Assign the argument to the private attribute
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
