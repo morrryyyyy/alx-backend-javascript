beca70ea4ab2d361f4bfaa4b98d0b4f3a9c4879e
Maryam <morrryyyy21@gmail.com>
2024-10-07 10:58:17 +0100

    edit constructor method

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,17 +1,20 @@
 export default class Building {
   constructor(sqft) {
-    this._sqft = sqft;
-    if (this.constructor === Building) {
-      if (typeof this.evacuationWarningMessage !== "function") {
+    this.sqft = sqft;
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
         throw new Error(
-          "Class extending Building must override evacuationWarningMessage"
+          'Class extending Building must override evacuationWarningMessage',
         );
       }
     }
   }
 
-  // getter method for sqft
   get sqft() {
     return this._sqft;
+  }
+
+  set sqft(value) {
+    this._sqft = value;
   }
 }
