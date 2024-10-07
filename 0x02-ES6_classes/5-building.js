1fe8297214dd2811e66a8bbcf3dacd6c7366d029
Maryam <morrryyyy21@gmail.com>
2024-10-07 09:32:08 +0100

    remove error message

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,5 @@
 export default class Building {
   constructor(sqft) {
-    /* If the current class is Building:
-            Throw Error: "Building is an abstract class and cannot be instantiated directly"
-        */
-    if (new.target === Building) {
-      throw new Error(
-        'Building is an abstract class and cannot be instantiated directly',
-      );
-    }
     this._sqft = sqft;
   }
 
@@ -21,7 +13,7 @@ export default class Building {
   // eslint-disable-next-line class-methods-use-this
   evacuationWarningMessage() {
     throw new Error(
-      'Class extending Building must override evacuationWarningMessage',
+      "Class extending Building must override evacuationWarningMessage"
     );
   }
 }
