3937420fd3660c5fd7c6a36a8e06653c3b0e3b47
Maryam <morrryyyy21@gmail.com>
2024-10-07 08:33:57 +0100

    create a new class with a method

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,35 +1,32 @@
 export default class Currency {
-  // Constructor to initialize the attributes
   constructor(code, name) {
-    this.code = code; // Use setters to validate
-    this.name = name;
+    this._code = code;
+    this._name = name;
   }
 
-  // Getter and Setter for code
+  // getter methods
+  // getter for code
   get code() {
     return this._code;
   }
 
-  set code(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Code must be a string');
-    }
-    this._code = value;
-  }
-
-  // Getter and Setter for name
+  // getter for name
   get name() {
     return this._name;
   }
 
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = value;
+  // setter methods
+  // setter for code
+  set code(newCode) {
+    this._code = newCode;
   }
 
-  // Method to display the full currency format: name (code)
+  // setter for name
+  set name(newName) {
+    this._name = newName;
+  }
+
+  // public method
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
