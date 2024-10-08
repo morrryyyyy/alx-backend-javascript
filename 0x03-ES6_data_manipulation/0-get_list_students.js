95f343d11538f52c6e515a5e5e9a429e56587ca4
Maryam <morrryyyy21@gmail.com>
2024-10-08 21:51:46 +0100

    create a function that returns an array

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function getListStudents() {
+  return [
+    { firstName: 'Guillaume', id: 1, location: 'San Fransisco' },
+    { firstName: 'James', id: 2, location: 'Columbia' },
+    { firstName: 'Serena', id: 5, location: 'San Fransisco' },
+  ];
+}
