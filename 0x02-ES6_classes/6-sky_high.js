99cd3f0ba1617e1c9401edb40a651c0a9e539989
Maryam <morrryyyy21@gmail.com>
2024-11-29 14:13:00 +0100

    dne

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -1,28 +1,8 @@
-// Import the Building class
-import Building from './5-building';
+import Building from "./5-building";
 
-export default class SkyHighBuilding extends Building {
-  // Constructor to initialize sqft and floors
+export default class skyHighBuilding extends Building {
   constructor(sqft, floors) {
-    super(sqft); // Pass sqft to the parent class (Building)
-    this.floors = floors; // Use setter for validation
-  }
-
-  // Getter for floors
-  get floors() {
-    return this._floors;
-  }
-
-  // Setter for floors with validation
-  set floors(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Floors must be a number');
-    }
-    this._floors = value;
-  }
-
-  // Override the evacuationWarningMessage method
-  evacuationWarningMessage() {
-    return `Evacuate slowly the ${this._floors} floors.`;
+    this._floors = floors;
+    super(sqft);
   }
 }
